f9d1a927cb5c5de1ed9b8d41f9d3491a6267fe69
Bruno Albrecht <bruno.albrecht@nanotempertech.com>
2021-01-22 19:57:52 +0100

    optimization
    constants from the image are now attributes of the class, so they don't need to be created/calculated again and again

--- yawk.py
+++ yawk.py
@@ -42,6 +42,8 @@ def get_config_data(file_path):
 
 
 class YAWK:
+    # BORDER, in pixels, so we don't draw to close to the edges
+    BORDER = 10
     def __init__(self):
         self.cfg_data = dict()
         cfg_file_data = get_config_data(CONFIGFILE)
@@ -71,6 +73,32 @@ class YAWK:
         except ValueError as e:
             print(e)
             fbink.fbink_close(self.fbfd)
+            
+        # configuration for the image
+        # Boxes positions
+        #   current condition
+        self.box_CURRENT = {'x': 0, 'y': 0, 'w': int(2 * self.screen_size[0] / 3), 'h': int(self.screen_size[1] / 3)}
+        #   today's forecast
+        self.box_TODAY = {'x': self.box_CURRENT['w'], 'y': 0, 'w': self.screen_size[0] - self.box_CURRENT['w'], 'h': self.box_CURRENT['h']}
+        #   tomorrow
+        self.box_TOMORROW = {'x': 0, 'y': self.box_CURRENT['h'], 'w': self.screen_size[0], 'h': int((self.screen_size[1] - self.box_CURRENT['h']) / 3)}
+        #   next 3 days
+        self.boxes_NEXT_DAYS = [{'x': 0, 'y': self.box_CURRENT['h'] + self.box_TOMORROW['h'], 'w': int(self.screen_size[0] / 3), 'h': self.screen_size[1] - self.box_CURRENT['h'] - self.box_TOMORROW['h']}]
+        self.boxes_NEXT_DAYS.append({'x': self.boxes_NEXT_DAYS[0]['w'], 'y': self.boxes_NEXT_DAYS[0]['y'], 'w': self.boxes_NEXT_DAYS[0]['w'], 'h': self.boxes_NEXT_DAYS[0]['h']})
+        self.boxes_NEXT_DAYS.append({'x': self.boxes_NEXT_DAYS[1]['x'] + self.boxes_NEXT_DAYS[1]['w'], 'y': self.boxes_NEXT_DAYS[1]['y'], 'w': self.boxes_NEXT_DAYS[1]['w'], 'h': self.boxes_NEXT_DAYS[1]['h']})
+        # fonts
+        #   used on the weather condition for the next days and ip address
+        self.font_tiny = ImageFont.truetype("fonts/Cabin-Regular.ttf", 22)
+        #   used on the headers and most stuff on the current conditions
+        self.font_small = ImageFont.truetype("fonts/Fabrica.otf", 26)
+        #   temperatures (gets scaled according to the box)
+        self.font_comfortaa = ImageFont.truetype("fonts/Comfortaa-Regular.ttf", 60)
+        #   for the current temperature
+        self.font_main_temperature = ImageFont.truetype("fonts/Comfortaa-Regular.ttf", int(self.screen_size[1] / 10))        
+        # icons
+        self.icon_celsius = Image.open('icons/C.png')
+        self.icon_wind = Image.open('icons/w.png')
+        self.icon_humidity = Image.open('icons/h.png')
 
     def _create_image(self):
 
@@ -91,139 +119,116 @@ class YAWK:
         black = 0
         gray = 128
 
-        BORDER = 10
-
-        # Boxes positions
-        # current condition
-        CURRENT = {'x': 0, 'y': 0, 'w': int(2 * WIDTH / 3), 'h': int(HEIGHT / 3)}
-        # today's forecast
-        TODAY = {'x': CURRENT['w'], 'y': 0, 'w': WIDTH - CURRENT['w'], 'h': CURRENT['h']}
-        # tomorrow
-        TOMORROW = {'x': 0, 'y': CURRENT['h'], 'w': WIDTH, 'h': int((HEIGHT - CURRENT['h']) / 3)}
-        # next 3 days
-        NEXT_DAYS = [{'x': 0, 'y': CURRENT['h'] + TOMORROW['h'], 'w': int(WIDTH / 3), 'h': HEIGHT - CURRENT['h'] - TOMORROW['h']}]
-        NEXT_DAYS.append({'x': NEXT_DAYS[0]['w'], 'y': NEXT_DAYS[0]['y'], 'w': NEXT_DAYS[0]['w'], 'h': NEXT_DAYS[0]['h']})
-        NEXT_DAYS.append({'x': NEXT_DAYS[1]['x'] + NEXT_DAYS[1]['w'], 'y': NEXT_DAYS[1]['y'], 'w': NEXT_DAYS[1]['w'], 'h': NEXT_DAYS[1]['h']})
-
         img = Image.new('L', (WIDTH, HEIGHT), color=white)
         draw = ImageDraw.Draw(img, 'L')
-        celsius_icon = Image.open('icons/C.png')
-        wind_icon = Image.open('icons/w.png')
-        humidity_icon = Image.open('icons/h.png')
-
-        tiny_font = ImageFont.truetype("fonts/Cabin-Regular.ttf", 22)
-        small_font = ImageFont.truetype("fonts/Fabrica.otf", 26)
-        font = ImageFont.truetype("fonts/Forum-Regular.ttf", 50)
-        comfortaa = ImageFont.truetype("fonts/Comfortaa-Regular.ttf", 60)
-        comfortaa_small = ImageFont.truetype("fonts/Comfortaa-Regular.ttf", 40)
 
         # Dividing lines
         # under today/current
-        draw.line([(BORDER, CURRENT['h']), (WIDTH - BORDER, CURRENT['h'])], gray)
+        draw.line([(self.BORDER, self.box_CURRENT['h']), (WIDTH - self.BORDER, self.box_CURRENT['h'])], gray)
         # between today/current
-        draw.line([(CURRENT['w'], BORDER), (CURRENT['w'], CURRENT['h'] - BORDER)], gray)
+        draw.line([(self.box_CURRENT['w'], self.BORDER), (self.box_CURRENT['w'], self.box_CURRENT['h'] - self.BORDER)], gray)
         # under tomorrow
-        draw.line([(BORDER, NEXT_DAYS[0]['y']), (WIDTH - BORDER, NEXT_DAYS[0]['y'])], gray)
+        draw.line([(self.BORDER, self.boxes_NEXT_DAYS[0]['y']), (WIDTH - self.BORDER, self.boxes_NEXT_DAYS[0]['y'])], gray)
         # between day 3/4
-        draw.line([(NEXT_DAYS[1]['x'], NEXT_DAYS[1]['y'] + BORDER), (NEXT_DAYS[1]['x'], HEIGHT - BORDER)], gray)
+        draw.line([(self.boxes_NEXT_DAYS[1]['x'], self.boxes_NEXT_DAYS[1]['y'] + self.BORDER), (self.boxes_NEXT_DAYS[1]['x'], HEIGHT - self.BORDER)], gray)
         # between day 4/5
-        draw.line([(NEXT_DAYS[2]['x'], NEXT_DAYS[2]['y'] + BORDER), (NEXT_DAYS[2]['x'], HEIGHT - BORDER)], gray)
+        draw.line([(self.boxes_NEXT_DAYS[2]['x'], self.boxes_NEXT_DAYS[2]['y'] + self.BORDER), (self.boxes_NEXT_DAYS[2]['x'], HEIGHT - self.BORDER)], gray)
 
         # Current conditions
         # City Name, Country Code, Day, Time
         header = self.current['city'] + ", " + datetime.now().strftime("%d.%m.%y, %Hh%M")
-        header_w, header_h = draw.textsize(header, font=small_font)
-        draw.text((CURRENT['w'] / 2 - header_w / 2, BORDER), header, font=small_font, fill=black)
+        header_w, header_h = draw.textsize(header, font=self.font_small)
+        draw.text((self.box_CURRENT['w'] / 2 - header_w / 2, self.BORDER), header, font=self.font_small, fill=black)
         # temperature
-        temp_font = ImageFont.truetype("fonts/Comfortaa-Regular.ttf", int(HEIGHT / 10))
-        temp_w, temp_h = draw.textsize(self.current['temperature'], font=temp_font)
-        draw.text((BORDER * 3, 2 * CURRENT['h'] / 7), self.current['temperature'], font=temp_font, fill=black)
+        
+        temp_w, temp_h = draw.textsize(self.current['temperature'], font=self.font_main_temperature)
+        draw.text((self.BORDER * 3, 2 * self.box_CURRENT['h'] / 7), self.current['temperature'], font=self.font_main_temperature, fill=black)
         # celsius
-        img.paste(celsius_icon.resize((celsius_icon.size[0] * 2, celsius_icon.size[1] * 2)), (BORDER * 3 + temp_w, int(2 * CURRENT['h'] / 7)))
+        img.paste(self.icon_celsius.resize((self.icon_celsius.size[0] * 2, self.icon_celsius.size[1] * 2)), (self.BORDER * 3 + temp_w, int(2 * self.box_CURRENT['h'] / 7)))
         # condition icon
         condition = Image.open(self.current['icon'])
         condition = condition.resize((int(condition.size[0] * 1.2), int(condition.size[1] * 1.2)))
-        temp_end_x = BORDER * 3 + temp_w + celsius_icon.size[0] * 2
-        x = int((CURRENT['w'] + temp_end_x) / 2 - condition.size[0] / 2)
-        img.paste(condition, (x, int(CURRENT['h'] / 2 - condition.size[1] / 2)))
+        temp_end_x = self.BORDER * 3 + temp_w + self.icon_celsius.size[0] * 2
+        x = int((self.box_CURRENT['w'] + temp_end_x) / 2 - condition.size[0] / 2)
+        img.paste(condition, (x, int(self.box_CURRENT['h'] / 2 - condition.size[1] / 2)))
         # condition description - under the icon?
-        condition_w, condition_h = draw.textsize(self.current['condition'], font=small_font)
-        x = (CURRENT['w'] + temp_end_x) / 2 - condition_w / 2
-        y = CURRENT['h'] / 2 + int(condition.size[1] / 2) + 3 * BORDER
-        draw.text((x, y), self.current['condition'], font=small_font, fill=gray)
+        condition_w, condition_h = draw.textsize(self.current['condition'], font=self.font_small)
+        x = (self.box_CURRENT['w'] + temp_end_x) / 2 - condition_w / 2
+        y = self.box_CURRENT['h'] / 2 + int(condition.size[1] / 2) + 3 * self.BORDER
+        draw.text((x, y), self.current['condition'], font=self.font_small, fill=gray)
         # wind icon
-        y = CURRENT['h'] - wind_icon.size[1]
-        img.paste(wind_icon, (BORDER, y))
+        y = self.box_CURRENT['h'] - self.icon_wind.size[1]
+        img.paste(self.icon_wind, (self.BORDER, y))
         # wind value
-        wind_w, wind_h = draw.textsize(self.current['wind_val'], font=small_font)
-        y = y + wind_icon.size[1] / 2 - wind_h / 2
-        draw.text((BORDER + wind_icon.size[0] + BORDER, y), self.current['wind_val'], font=small_font, fill=black)
+        wind_w, wind_h = draw.textsize(self.current['wind_val'], font=self.font_small)
+        y = y + self.icon_wind.size[1] / 2 - wind_h / 2
+        draw.text((self.BORDER + self.icon_wind.size[0] + self.BORDER, y), self.current['wind_val'], font=self.font_small, fill=black)
         # humidity icon
-        y = CURRENT['h'] - wind_icon.size[1] - humidity_icon.size[1]
-        x = int(BORDER + wind_icon.size[0] / 2 - humidity_icon.size[0] / 2)
-        img.paste(humidity_icon, (x, y))
+        y = self.box_CURRENT['h'] - self.icon_wind.size[1] - self.icon_humidity.size[1]
+        x = int(self.BORDER + self.icon_wind.size[0] / 2 - self.icon_humidity.size[0] / 2)
+        img.paste(self.icon_humidity, (x, y))
         # humidity value
-        humidity_w, humidity_h = draw.textsize(self.current['humidity'], font=small_font)
-        y = y + humidity_icon.size[1] / 2 - humidity_h / 2
-        draw.text((BORDER + wind_icon.size[0] + BORDER, y), self.current['humidity'], font=small_font, fill=black)
+        humidity_w, humidity_h = draw.textsize(self.current['humidity'], font=self.font_small)
+        y = y + self.icon_humidity.size[1] / 2 - humidity_h / 2
+        draw.text((self.BORDER + self.icon_wind.size[0] + self.BORDER, y), self.current['humidity'], font=self.font_small, fill=black)
 
         def print_temp(pos, text, temp, scale=1.0):
             # text string
-            text_w, text_h = draw.textsize(text, font=small_font)
+            text_w, text_h = draw.textsize(text, font=self.font_small)
             y = pos[1] - text_h
             x = pos[0]
-            draw.text((x, y), text, font=small_font, fill=gray)
-            # low value
-            temp_width, temp_height = draw.textsize(temp, font=comfortaa)
+            draw.text((x, y), text, font=self.font_small, fill=gray)
+            # value
+            temp_width, temp_height = draw.textsize(temp, font=self.font_comfortaa)
             y = y + text_h - temp_height
             x += text_w
-            draw.text((x, y), temp, font=comfortaa, fill=black)
+            draw.text((x, y), temp, font=self.font_comfortaa, fill=black)
             # celsius
             x += temp_width
-            img.paste(celsius_icon.resize((int(celsius_icon.size[0] * scale), int(celsius_icon.size[1] * scale))), (int(x), int(y)))
+            img.paste(self.icon_celsius.resize((int(self.icon_celsius.size[0] * scale), int(self.icon_celsius.size[1] * scale))), (int(x), int(y)))
 
         # today's forecast
         # low temperature
-        position = [TODAY['x'] + BORDER, TODAY['h'] / 4]
+        position = [self.box_TODAY['x'] + self.BORDER, self.box_TODAY['h'] / 4]
         print_temp(position, "low: ", today['low'], 1.3)
         # high temperature
-        position = [TODAY['x'] + BORDER, 2 * TODAY['h'] / 4]
+        position = [self.box_TODAY['x'] + self.BORDER, 2 * self.box_TODAY['h'] / 4]
         print_temp(position, "high: ", today['high'], 1.3)
         # condition icon
         condition = Image.open(today['icon'])
-        y = int(3 * TODAY['h'] / 4 - condition.size[1] / 2)
-        x = int(TODAY['x'] + TODAY['w'] / 2 - condition.size[0] / 2)
+        y = int(3 * self.box_TODAY['h'] / 4 - condition.size[1] / 2)
+        x = int(self.box_TODAY['x'] + self.box_TODAY['w'] / 2 - condition.size[0] / 2)
         img.paste(condition, (x, y))
 
         # tomorrow's forecast
         # tomorrow's text
-        draw.text((TOMORROW['x'] + BORDER, TOMORROW['y'] + BORDER), tomorrow['day'], font=small_font, fill=black)
+        draw.text((self.box_TOMORROW['x'] + self.BORDER, self.box_TOMORROW['y'] + self.BORDER), tomorrow['day'], font=self.font_small, fill=black)
         # low
-        x = int(TOMORROW['w'] / 8)
-        y = int(TOMORROW['y'] + 2 * TOMORROW['h'] / 3)
+        x = int(self.box_TOMORROW['w'] / 8)
+        y = int(self.box_TOMORROW['y'] + 2 * self.box_TOMORROW['h'] / 3)
         print_temp((x, y), "low: ", tomorrow['low'], 1.5)
         # high
-        x = int(2 * TOMORROW['w'] / 3)
+        x = int(2 * self.box_TOMORROW['w'] / 3)
         print_temp((x, y), "high: ", tomorrow['high'], 1.5)
         # condition icon
         condition = Image.open(tomorrow['icon'])
         y -= condition.size[1]
-        x = int(TOMORROW['w'] / 2 - condition.size[0] / 2)
+        x = int(self.box_TOMORROW['w'] / 2 - condition.size[0] / 2)
         img.paste(condition, (x, y))
         # condition description - under the icon
-        condition_w, condition_h = draw.textsize(tomorrow['condition'], font=small_font)
-        x = int(TOMORROW['w'] / 2 - condition_w / 2)
-        y += condition.size[1] + 2 * BORDER
-        draw.text((x, y), tomorrow['condition'], font=small_font, fill=gray)
+        condition_w, condition_h = draw.textsize(tomorrow['condition'], font=self.font_small)
+        x = int(self.box_TOMORROW['w'] / 2 - condition_w / 2)
+        y += condition.size[1] + 2 * self.BORDER
+        draw.text((x, y), tomorrow['condition'], font=self.font_small, fill=gray)
 
         def print_other_days(dimensions, data):
             # day name
-            day_w, day_h = draw.textsize(data['day'], font=small_font)
+            day_w, day_h = draw.textsize(data['day'], font=self.font_small)
             x = int(dimensions['x'] + dimensions['w'] / 2 - day_w / 2)
-            y = dimensions['y'] + BORDER
-            draw.text((x, y), data['day'], font=small_font, fill=black)
+            y = dimensions['y'] + self.BORDER
+            draw.text((x, y), data['day'], font=self.font_small, fill=black)
             # low temp
-            x = dimensions['x'] + BORDER
+            x = dimensions['x'] + self.BORDER
             y = int(dimensions['y'] + dimensions['h'] / 4)
             print_temp((x, y), "low: ", data['low'])
             # high temp
@@ -231,22 +236,22 @@ class YAWK:
             print_temp((x, y), "high: ", data['high'])
             # condition icon
             condition = Image.open(data['icon'])
-            y += 2 * BORDER
+            y += 2 * self.BORDER
             x = int(dimensions['x'] + dimensions['w'] / 2 - condition.size[0] / 2)
             img.paste(condition, (x, y))
             # condition description
-            condition_w, condition_h = draw.textsize(data['condition'], font=tiny_font)
-            y += condition.size[1] + BORDER
+            condition_w, condition_h = draw.textsize(data['condition'], font=self.font_tiny)
+            y += condition.size[1] + self.BORDER
             x = int(dimensions['x'] + dimensions['w'] / 2 - condition_w / 2)
-            draw.text((x, y), data['condition'], font=tiny_font, fill=gray)
+            draw.text((x, y), data['condition'], font=self.font_tiny, fill=gray)
 
         # the next 3 days
         for i in range(3):
-            print_other_days(NEXT_DAYS[i], days[i])
+            print_other_days(self.boxes_NEXT_DAYS[i], days[i])
 
         # ip address
-        ip_w, ip_h = draw.textsize(self.ip_address, font=tiny_font)
-        draw.text((WIDTH - BORDER - ip_w, HEIGHT - BORDER - ip_h), self.ip_address, font=tiny_font, fill=gray)
+        ip_w, ip_h = draw.textsize(self.ip_address, font=self.font_tiny)
+        draw.text((WIDTH - self.BORDER - ip_w, HEIGHT - self.BORDER - ip_h), self.ip_address, font=self.font_tiny, fill=gray)
 
         if "linux" in platform:
             img.save(tempfile.gettempdir() + "/img.bmp")
@@ -286,12 +291,16 @@ def main():
         call(["killall", "-TERM", "nickel", "hindenburg", "sickel", "fickel"])
 
     yawk = YAWK()
+    counter = 0
 
     try:
         while True:
+            print("*** Updating at " + datetime.now().strftime("%d.%m.%y, %Hh%M") + " (update nr. " + str(counter) + ") ***")
             yawk.ip_address = wait_for_wifi()
             yawk.update()
+            print("Sleeping")
             time.sleep(5 * 60)
+            counter += 1
     finally:
         fbink.fbink_close(yawk.fbfd)
 
